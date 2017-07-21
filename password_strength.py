aeb131da4d3a6c1fa4833b0e173d33c29b502341
antondemkin <antondemkin@yandex.ru>
2017-07-22 01:16:01 +0300

    Code improved

--- password_strength.py
+++ password_strength.py
@@ -1,43 +1,39 @@
-'''
-string is nice, but re is better! :-)
-'''
-
 import re
+import getpass
 
 
 def get_password_strength(password):
-    # checklist:
-    # len8   (.){8,}
-    # len15  (.){15,}
-    # len25  (.){25,}
-    # worst - string only :-/
-    # not years  "^(?!.*(1|2)(\d)(\d)(\d)).*"
-    # digit  \d
-    # special    [^a-zA-Z0-9]
-    # uppercase  [A-Z]
-    # lowercase  [a-z]
-    # NO repeated symbols   ^(?!.*(.)\1\1\1\1).*
+    checklist = [r"\d",                         # contain digit
+                 r"[^a-zA-Z0-9]",               # contain special symbol (not letter and not digit)
+                 r"[a-z]",                      # contain lowercase letter
+                 r"[A-Z]",                      # contain uppercase letter
+                 r"^(?!.*(.)\1\1\1\1).*",       # does not contain symbols repeated more than 4 times
+                 r"^(?!.*(1|2)(\d)(\d)(\d)).*"] # does not contain a year (four digits, started with 1 or 2)
     
-    checklist = [r"(.){5,}", r"(.){12,}", r"(.){20,}", r"\d", r"[^a-zA-Z0-9]", r"[a-z]", r"[A-Z]",
-                 r"^(?!.*(.)\1\1\1\1).*",
-                 r"^(?!.*(1|2)(\d)(\d)(\d)).*"]
-    worst_passwords = '123456 password 12345678 qwerty 12345 123456789 football 1234567 baseball welcome 1234567890 ' \
-            'abc123 111111 1qaz2wsx dragon master monkey letmein login princess qwertyuiop solo passw0rd starwars'
+    with open('worst_passwords.txt', 'r') as file:
+        worst_passwords = file.read()
     
-    security_levels = set()
+    security_levels = []
+    
+    if len(password) > 5:
+        security_levels.append('len5')
+    if len(password) > 12:
+        security_levels.append('len12')
+    if len(password) > 20:
+        security_levels.append('len20')
     
     for regexp in checklist:
         if re.search(regexp, password) is not None:
-            security_levels.add(regexp)
+            security_levels.append(regexp)
     
     if password not in worst_passwords:
-        security_levels.add('not in worst')
+        security_levels.append('not in worst')
     
     return len(security_levels)
 
 
 def main():
-    password = input('Enter password: ')
+    password = getpass.getpass()
     print(get_password_strength(password))
 
 

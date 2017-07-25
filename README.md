b09ff73dcc69e1b8645d110489f3d6298f48adae
antondemkin <antondemkin@yandex.ru>
2017-07-25 16:41:14 +0300

    Now it will get list of 10k worst passwords for you.

--- README.md
+++ README.md
@@ -6,9 +6,9 @@ antondemkin@yandex.ru
 
 # Usage:
 
-Before first launch you need to download actual list of worst passwords from [here](https://github.com/danielmiessler/SecLists/tree/master/Passwords)
-You need any file, for example 10_million_password_list_top_1000.txt,
-rename it to **passwords.txt** and place into the same directory as script.
+Script require internet connection.
+It will automatically download [this](https://github.com/danielmiessler/SecLists/blob/master/Passwords/10_million_password_list_top_1000.txt)
+list of internet worst passwords. If unable to download (no internet...) it will skip this step and still run.
 
 How to run:
 ```
--- password_strength.py
+++ password_strength.py
@@ -1,19 +1,21 @@
 import re
 import getpass
+import urllib.request
+import ssl
 
 
 def get_password_strength(password):
-    checklist = [r"(.){5,}",                    # length more than 5, 12 or 20
+    checklist = [r"(.){5,}",  # length more than 5, 12 or 20
                  r"(.){12,}",
                  r"(.){20,}",
-                 r"\d",                         # contain digit
-                 r"[^a-zA-Z0-9]",               # contain special symbol (not letter and not digit)
-                 r"[a-z]",                      # contain lowercase letter
-                 r"[A-Z]",                      # contain uppercase letter
-                 r"^(?!.*(.)\1\1\1\1).*",       # does not contain symbols repeated more than 4 times
-                 r"^(?!.*(1|2)(\d)(\d)(\d)).*"] # does not contain a year (four digits, started with 1 or 2)
+                 r"\d",  # contain digit
+                 r"[^a-zA-Z0-9]",  # contain special symbol (not letter and not digit)
+                 r"[a-z]",  # contain lowercase letter
+                 r"[A-Z]",  # contain uppercase letter
+                 r"^(?!.*(.)\1\1\1\1).*",  # does not contain symbols repeated more than 4 times
+                 r"^(?!.*(1|2)(\d)(\d)(\d)).*"]  # does not contain a year (four digits, started with 1 or 2)
     
-    worst_passwords = load_worst_passwords()
+    worst_passwords = download_worst_passwords()
     
     security_levels = []
     
@@ -26,15 +28,21 @@ def get_password_strength(password):
     
     return len(security_levels)
 
-def load_worst_passwords():
-    try:
-        with open('passwords1.txt', 'r') as file:
-            worst_passwords = file.read()
-        return worst_passwords
-    except FileNotFoundError:
-        print('Please put passwords.txt to script directory. Checking for worst passwords is disabled.')
-        return []
 
+def download_worst_passwords():
+    try:
+        context = ssl._create_unverified_context()
+        url = 'https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords' \
+              '/10_million_password_list_top_10000.txt'
+        content = urllib.request.urlopen(url, context=context)
+        list_of_passwords = str(content.read())
+        
+    except urllib.error.HTTPError as error:
+        print("Failed to fetch passwords list:", error)
+        list_of_passwords = []
+        
+    finally:
+        return list_of_passwords
 
 
 def main():
